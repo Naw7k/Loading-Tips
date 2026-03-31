561c8def98afc23d4cbf68905876ca0dfc29ce33
Naw7k <n_m_k0@hotmail.com>
2026-03-31 14:28:21 +0300

    Update and rename src/main/java/net/naw/custom_loading_tips/client/Custom_loading_tipsClient.java to src/main/java/net/naw/loading_tips/Loading_tipsClient.java

---
+++ src/main/java/net/naw/loading_tips/Loading_tipsClient.java
@@ -0,0 +1,10 @@
+package net.naw.loading_tips.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Loading_tipsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
